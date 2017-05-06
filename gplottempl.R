8890b7bf045f8918244f0fd9ed88227489d3a007
uswick <mastershield2007@gmail.com>
2017-05-06 03:01:14 -0400

    add scatter plot support

--- gplottempl.R
+++ gplottempl.R
@@ -1,5 +1,100 @@
 # simple plotting framework for R using ggplot2
 # Author : Udayanga Wickramasinghe [uswickra@umail.iu.edu]
+
+
+############### START Scatter Plots ################
+myplot_scatter = function(dataset, x_string, y_string, color_string, title="") {
+  # assign(sup_var, dataset.)
+  # Black error bars - notice the mapping of 'group=supp' -- without it, the error
+  # bars won't be dodged!
+  try(
+    if(!exists("MYPLOT_CONSTANTS_DEF")){ 
+      stop("Fatal Error! Constant file consts.R not available!")
+      return()
+    }
+  )
+  
+  if(MYPLOT_PROPS['legend.key', 'active']){
+    loc.lg_key = element_rect()
+  }else{
+    loc.lg_key = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['legend.title', 'active']){
+    loc.lg_title = element_text()
+  }else{
+    loc.lg_title = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['plot.title.global', 'active']){
+    loc.plt_title = Titl
+  }else{
+    loc.plt_title = title
+  }
+  
+  if(MYPLOT_PROPS['axis.text', 'active']){
+    loc.ax_txt =  element_text(face=MYPLOT_PROPS['axis.text', 'face'], size=MYPLOT_PROPS['axis.text', 'size'])
+  }else{
+    loc.ax_txt = element_text()
+  }
+  
+  if(MYPLOT_PROPS['axis.range.y', 'active']){
+    loc.ylimits = c(MYPLOT_PROPS['axis.range.y.limit', 'a'], MYPLOT_PROPS['axis.range.y.limit', 'b'])  
+  }else{
+    loc.ylimits = NULL
+  }
+  if(MYPLOT_PROPS['axis.range.x', 'active']){
+    loc.xbreaks = seq(MYPLOT_PROPS['axis.range.x.break', 'a'], MYPLOT_PROPS['axis.range.x.break', 'b'], MYPLOT_PROPS['axis.range.x.break', 'step'])  
+  }else{
+    loc.xbreaks = NULL
+  }
+  
+  ydata <- dataset[y_string]
+  
+  p1 <- ggplot(dataset, aes_string(x=x_string, y=y_string, colour=color_string, group=color_string)) +
+    # geom_point(position=pd, size=1) +
+    geom_point(aes_string(shape=color_string, fill=color_string, color=color_string), size=MYPLOT_PROPS['geom_point', 'units']) +  # add a scatterplot; constant size, shape/fill depends on lesion
+    geom_smooth(method="loess", se=TRUE, fullrange=FALSE, level=0.99, aes_string(fill=color_string), size=0.28)+
+    # geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95, size=0.4)+
+    # scale_x_continuous(X_LABEL) + # have tick marks for each session
+    scale_x_continuous(X_LABEL, breaks = loc.xbreaks) + # have tick marks for each session
+    #with log scale
+    # scale_x_continuous(X_LABEL, breaks = loc.xbreaks, trans="log2") + # have tick marks for each session
+    
+    # scale_y_continuous(Y_LABEL) + # have tick marks for each session
+    scale_y_continuous(Y_LABEL, limits = loc.ylimits ) + # have tick marks for each session
+    scale_shape_manual(values=SERIES_MARKERS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_color_manual(values=SERIES_COLORS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_fill_manual(values=SERIES_FILL_COLORS) + # explicitly have sham=white, ACCX=black
+    #scale_fill_manual(values=c("#CC0000", "#006600", "#669999", "#00CCCC",
+    #               "#660099", "#CC0066", "#FF9999", "#FF9900", 
+    #                "black", "black", "black", "black", "black")) +
+    theme_bw()+  # make the theme black-and-white rather than grey (do this before font changes, or it overrides them)
+    ggtitle(loc.plt_title)+
+    theme(
+      plot.title = element_text(face=MYPLOT_PROPS['plot.title', 'face'], size=MYPLOT_PROPS['plot.title', 'size']), # use theme_get() to see available options
+      axis.title.x = element_text(face=MYPLOT_PROPS['axis.title.x', 'face'], size=MYPLOT_PROPS['axis.title.x', 'size']),
+      axis.title.y = element_text(face=MYPLOT_PROPS['axis.title.y', 'face'], size=MYPLOT_PROPS['axis.title.y', 'size'], angle=MYPLOT_PROPS['axis.title.y', 'angle']),
+      axis.text = loc.ax_txt, 
+      # panel.grid.major = element_blank(), # switch off major gridlines
+      # panel.grid.minor = element_blank(), # switch off minor gridlines
+      legend.position = LEGEND_POS, # manually position the legend (numbers being from 0,0 at bottom left of whole plot to 1,1 at top right)
+      legend.title = loc.lg_title, # switch off the legend title
+      legend.text = element_text(size=MYPLOT_PROPS['legend.text', 'size']),
+      legend.key.size = unit(MYPLOT_PROPS['legend.key', 'units'], "lines"),
+      legend.key = loc.lg_key, # switch off the rectangle around symbols in the legend
+      legend.background = element_rect(fill=alpha('white', 0.2))
+    )
+  
+  return(p1)
+}
+
+
+############### END Scatter Plots ################
+
+
+############### START Line Plots ################
+
 myplot_shrink_descrete = function(dataset, x_string, y_string, color_string, error_string, title="") {
   # assign(sup_var, dataset.)
   # Black error bars - notice the mapping of 'group=supp' -- without it, the error
