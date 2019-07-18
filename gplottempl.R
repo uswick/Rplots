e6025a1003dbd8625ba72692e89fc1e73a3a6681
Udayanga Wickramasinghe <uswickra@umail.iu.edu>
2019-07-18 01:44:31 -0700

    add templ

--- gplottempl.R
+++ gplottempl.R
@@ -95,7 +95,182 @@ myplot_scatter = function(dataset, x_string, y_string, color_string, title="") {
 
 ############### START Line Plots ################
 myplot_shrink_descrete_wlabels = function(dataset, x_string, y_string, color_string, error_string, title="",
-                                          xlabels=c("24","48","96","192","384","768","1.5K","3K","6K","12K","24K","8K","16K","32K","64K","128K","256K","512K","1M", "2M")) {
+                                          xlabels=c("24","48","96","192","384","768","1.5K","3K","6K","12K","24K","8K","16K","32K","64K","128K","256K","512K","1M", "2M")
+                                          ,xaxisname="[test name]") {
+  # assign(sup_var, dataset.)
+  # Black error bars - notice the mapping of 'group=supp' -- without it, the error
+  # bars won't be dodged!
+  try(
+    if(!exists("MYPLOT_CONSTANTS_DEF")){ 
+      stop("Fatal Error! Constant file consts.R not available!")
+      return()
+    }
+  )
+  
+  if(MYPLOT_PROPS['legend.key', 'active']){
+    loc.lg_key = element_rect()
+  }else{
+    loc.lg_key = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['legend.title', 'active']){
+    loc.lg_title = element_text()
+  }else{
+    loc.lg_title = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['plot.title.global', 'active']){
+    loc.plt_title = Titl
+  }else{
+    loc.plt_title = title
+  }
+  
+  if(MYPLOT_PROPS['axis.text', 'active']){
+    loc.ax_txt =  element_text(face=MYPLOT_PROPS['axis.text', 'face'], size=MYPLOT_PROPS['axis.text', 'size'])
+  }else{
+    loc.ax_txt = element_text()
+  }
+  
+  if(MYPLOT_PROPS['axis.range.y', 'active']){
+    loc.ylimits = c(MYPLOT_PROPS['axis.range.y.limit', 'a'], MYPLOT_PROPS['axis.range.y.limit', 'b'])  
+  }else{
+    loc.ylimits = NULL
+  }
+  
+  pd <- position_dodge(0.1) # move them .05 to the left and right
+  ydata <- dataset[y_string]
+  if(!is.null(error_string)){
+    error <- dataset[error_string]
+  } else{
+    error <- c(rep(0, nrow(dataset)))
+  }
+  
+  p1 <- ggplot(dataset, aes_string(x=x_string, y=y_string, colour=color_string, group=color_string)) +
+    geom_errorbar(aes(ymin=ydata-error, ymax=ydata+error), colour="black", width=0.3, position=pd, size=0.4, linetype='solid') +
+    geom_line(position=pd, size=0.6) +
+    # geom_point(position=pd, size=1) +
+    geom_point(aes_string(shape=color_string, fill=color_string, color=color_string), position=pd, size=MYPLOT_PROPS['geom_point', 'units']) +  # add a scatterplot; constant size, shape/fill depends on lesion
+    # scale_y_continuous(Y_LABEL) + # have tick marks for each session
+    scale_x_discrete(xaxisname,labels=xlabels)+
+    scale_y_continuous(Y_LABEL, limits = loc.ylimits) + # have tick marks for each session
+    scale_shape_manual(values=SERIES_MARKERS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_color_manual(values=SERIES_COLORS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_fill_manual(values=SERIES_FILL_COLORS) + # explicitly have sham=white, ACCX=black
+    #scale_fill_manual(values=c("#CC0000", "#006600", "#669999", "#00CCCC",
+    #               "#660099", "#CC0066", "#FF9999", "#FF9900", 
+    #                "black", "black", "black", "black", "black")) +
+    theme_bw()+  # make the theme black-and-white rather than grey (do this before font changes, or it overrides them)
+    ggtitle(loc.plt_title)+
+    theme(
+      plot.title = element_text(hjust = 0.5, face=MYPLOT_PROPS['plot.title', 'face'], size=MYPLOT_PROPS['plot.title', 'size']), # use theme_get() to see available options
+      axis.title.x = element_text(face=MYPLOT_PROPS['axis.title.x', 'face'], size=MYPLOT_PROPS['axis.title.x', 'size']),
+      axis.title.y = element_text(face=MYPLOT_PROPS['axis.title.y', 'face'], size=MYPLOT_PROPS['axis.title.y', 'size'], angle=MYPLOT_PROPS['axis.title.y', 'angle']),
+      axis.text = loc.ax_txt, 
+      # axis.text.x = MYPLOT_PROPS['axis.text', 'x'],
+      # panel.grid.major = element_blank(), # switch off major gridlines
+      # panel.grid.minor = element_blank(), # switch off minor gridlines
+      legend.position = LEGEND_POS, # manually position the legend (numbers being from 0,0 at bottom left of whole plot to 1,1 at top right)
+      legend.title = loc.lg_title, # switch off the legend title
+      legend.text = element_text(size=MYPLOT_PROPS['legend.text', 'size']),
+      legend.key.size = unit(MYPLOT_PROPS['legend.key', 'units'], "lines"),
+      legend.key = loc.lg_key, # switch off the rectangle around symbols in the legend
+      legend.background = element_rect(fill=alpha('white', 0.2))
+    )
+  
+  return(p1)
+}
+
+myplot_shrink_descrete_wlabels_cguide = function(dataset, x_string, y_string, color_string, error_string, title="",
+                                          xlabels=c("24","48","96","192","384","768","1.5K","3K","6K","12K","24K","8K","16K","32K","64K","128K","256K","512K","1M", "2M")
+                                          ,xaxisname="[test name]") {
+  # assign(sup_var, dataset.)
+  # Black error bars - notice the mapping of 'group=supp' -- without it, the error
+  # bars won't be dodged!
+  try(
+    if(!exists("MYPLOT_CONSTANTS_DEF")){ 
+      stop("Fatal Error! Constant file consts.R not available!")
+      return()
+    }
+  )
+  
+  if(MYPLOT_PROPS['legend.key', 'active']){
+    loc.lg_key = element_rect()
+  }else{
+    loc.lg_key = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['legend.title', 'active']){
+    loc.lg_title = element_text()
+  }else{
+    loc.lg_title = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['plot.title.global', 'active']){
+    loc.plt_title = Titl
+  }else{
+    loc.plt_title = title
+  }
+  
+  if(MYPLOT_PROPS['axis.text', 'active']){
+    loc.ax_txt =  element_text(face=MYPLOT_PROPS['axis.text', 'face'], size=MYPLOT_PROPS['axis.text', 'size'])
+  }else{
+    loc.ax_txt = element_text()
+  }
+  
+  if(MYPLOT_PROPS['axis.range.y', 'active']){
+    loc.ylimits = c(MYPLOT_PROPS['axis.range.y.limit', 'a'], MYPLOT_PROPS['axis.range.y.limit', 'b'])  
+  }else{
+    loc.ylimits = NULL
+  }
+  
+  pd <- position_dodge(0.1) # move them .05 to the left and right
+  ydata <- dataset[y_string]
+  if(!is.null(error_string)){
+    error <- dataset[error_string]
+  } else{
+    error <- c(rep(0, nrow(dataset)))
+  }
+  
+  p1 <- ggplot(dataset, aes_string(x=x_string, y=y_string, colour=color_string, group=color_string)) +
+    geom_errorbar(aes(ymin=ydata-error, ymax=ydata+error), colour="black", width=0.7, position=pd, size=0.28, linetype='solid') +
+    geom_line(position=pd, size=0.6) +
+    # geom_point(position=pd, size=1) +
+    geom_point(aes_string(shape=color_string, fill=color_string), position=pd, size=MYPLOT_PROPS['geom_point', 'units']) +  # add a scatterplot; constant size, shape/fill depends on lesion
+    # scale_y_continuous(Y_LABEL) + # have tick marks for each session
+    scale_x_discrete(xaxisname,labels=xlabels)+
+    scale_y_continuous(Y_LABEL, limits = loc.ylimits) + # have tick marks for each session
+    #scale_shape_manual(values=SERIES_MARKERS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_color_manual(values=SERIES_COLORS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_fill_manual(values=SERIES_FILL_COLORS) + # explicitly have sham=white, ACCX=black
+    #scale_fill_manual(values=c("#CC0000", "#006600", "#669999", "#00CCCC",
+    #               "#660099", "#CC0066", "#FF9999", "#FF9900", 
+    #                "black", "black", "black", "black", "black")) +
+    theme_bw()+  # make the theme black-and-white rather than grey (do this before font changes, or it overrides them)
+    ggtitle(loc.plt_title)+
+    theme(
+      plot.title = element_text(hjust = 0.5, face=MYPLOT_PROPS['plot.title', 'face'], size=MYPLOT_PROPS['plot.title', 'size']), # use theme_get() to see available options
+      axis.title.x = element_text(face=MYPLOT_PROPS['axis.title.x', 'face'], size=MYPLOT_PROPS['axis.title.x', 'size']),
+      axis.title.y = element_text(face=MYPLOT_PROPS['axis.title.y', 'face'], size=MYPLOT_PROPS['axis.title.y', 'size'], angle=MYPLOT_PROPS['axis.title.y', 'angle']),
+      axis.text = loc.ax_txt, 
+      # axis.text.x = MYPLOT_PROPS['axis.text', 'x'],
+      # panel.grid.major = element_blank(), # switch off major gridlines
+      # panel.grid.minor = element_blank(), # switch off minor gridlines
+      legend.position = LEGEND_POS, # manually position the legend (numbers being from 0,0 at bottom left of whole plot to 1,1 at top right)
+      legend.title = loc.lg_title, # switch off the legend title
+      legend.text = element_text(size=MYPLOT_PROPS['legend.text', 'size']),
+      legend.key.size = unit(MYPLOT_PROPS['legend.key', 'units'], "lines"),
+      legend.key = loc.lg_key, # switch off the rectangle around symbols in the legend
+      legend.background = element_rect(fill=alpha('white', 0.2))
+    )
+  
+  return(p1)
+}
+
+
+
+myplot_shrink_descrete_wlabels2 = function(dataset, x_string, y_string, color_string, error_string, title="",
+                                          xlabels=c("24","48","96","192","384","768","1.5K","3K","6K","12K","24K","8K","16K","32K","64K","128K","256K","512K","1M", "2M"),
+                                          xtitle ="Message Size (bytes)") {
   # assign(sup_var, dataset.)
   # Black error bars - notice the mapping of 'group=supp' -- without it, the error
   # bars won't be dodged!
@@ -150,7 +325,7 @@ myplot_shrink_descrete_wlabels = function(dataset, x_string, y_string, color_str
     # geom_point(position=pd, size=1) +
     geom_point(aes_string(shape=color_string, fill=color_string, color=color_string), position=pd, size=MYPLOT_PROPS['geom_point', 'units']) +  # add a scatterplot; constant size, shape/fill depends on lesion
     # scale_y_continuous(Y_LABEL) + # have tick marks for each session
-    scale_x_discrete(aes(breaks = "Message Size (bytes)"),labels=xlabels)+
+    scale_x_discrete(aes(breaks = "Nodes (N)"),labels=xlabels)+
     scale_y_continuous(Y_LABEL, limits = loc.ylimits) + # have tick marks for each session
     scale_shape_manual(values=SERIES_MARKERS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
     scale_color_manual(values=SERIES_COLORS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
@@ -159,7 +334,7 @@ myplot_shrink_descrete_wlabels = function(dataset, x_string, y_string, color_str
     #               "#660099", "#CC0066", "#FF9999", "#FF9900", 
     #                "black", "black", "black", "black", "black")) +
     theme_bw()+  # make the theme black-and-white rather than grey (do this before font changes, or it overrides them)
-    ggtitle(loc.plt_title)+
+    ggtitle(loc.plt_title)+  
     theme(
       plot.title = element_text(hjust = 0.5, face=MYPLOT_PROPS['plot.title', 'face'], size=MYPLOT_PROPS['plot.title', 'size']), # use theme_get() to see available options
       axis.title.x = element_text(face=MYPLOT_PROPS['axis.title.x', 'face'], size=MYPLOT_PROPS['axis.title.x', 'size']),
@@ -174,11 +349,10 @@ myplot_shrink_descrete_wlabels = function(dataset, x_string, y_string, color_str
       legend.key.size = unit(MYPLOT_PROPS['legend.key', 'units'], "lines"),
       legend.key = loc.lg_key, # switch off the rectangle around symbols in the legend
       legend.background = element_rect(fill=alpha('white', 0.2))
-    )
+    ) 
   
   return(p1)
 }
-
 
 
 
@@ -754,6 +928,103 @@ myplot_bar = function(dataset, x_string, y_string, color_string, error_string, t
   
   return(p1)
 }
+
+
+myplot_bar_def = function(dataset, x_string, y_string, color_string, error_string, title="") {
+  # assign(sup_var, dataset.)
+  # Black error bars - notice the mapping of 'group=supp' -- without it, the error
+  # bars won't be dodged!
+  try(
+    if(!exists("MYPLOT_CONSTANTS_DEF")){ 
+      stop("Fatal Error! Constant file consts.R not available!")
+      return()
+    }
+  )
+  
+  if(MYPLOT_PROPS['legend.key', 'active']){
+    loc.lg_key = element_rect()
+  }else{
+    loc.lg_key = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['legend.title', 'active']){
+    loc.lg_title = element_text()
+  }else{
+    loc.lg_title = element_blank()
+  }
+  
+  if(MYPLOT_PROPS['plot.title.global', 'active']){
+    loc.plt_title = Titl
+  }else{
+    loc.plt_title = title
+  }
+  
+  if(MYPLOT_PROPS['axis.text', 'active']){
+    loc.ax_txt =  element_text(face=MYPLOT_PROPS['axis.text', 'face'], size=MYPLOT_PROPS['axis.text', 'size'])
+  }else{
+    loc.ax_txt = element_text()
+  }
+  
+  if(MYPLOT_PROPS['axis.range.y', 'active']){
+    loc.ylimits = c(MYPLOT_PROPS['axis.range.y.limit', 'a'], MYPLOT_PROPS['axis.range.y.limit', 'b'])  
+  }else{
+    loc.ylimits = NULL
+  }
+  if(MYPLOT_PROPS['axis.range.x', 'active']){
+    loc.xbreaks = seq(MYPLOT_PROPS['axis.range.x.break', 'a'], MYPLOT_PROPS['axis.range.x.break', 'b'], MYPLOT_PROPS['axis.range.x.break', 'step'])  
+  }else{
+    loc.xbreaks = NULL
+  }
+  
+  pd <- position_dodge(0.9) # move them .05 to the left and right
+  ydata <- dataset[y_string]
+  if(!is.null(error_string)){
+    error <- dataset[error_string]
+  } else{
+    error <- c(rep(0, nrow(dataset)))
+  }
+  
+  p1 <- ggplot(dataset, aes_string(x=x_string, y=y_string, fill=color_string)) +
+    #geom_errorbar(aes(ymin=ydata-error, ymax=ydata+error), colour="black", width=1.5, position=pd, size=0.28, linetype='solid') +
+    #geom_bar(stat="identity", position=position_dodge(), colour="black") +
+    geom_bar(stat="identity", position=position_dodge(), colour="black",width=0.9)+
+    #geom_bar(stat="identity")+
+    # scale_x_continuous(X_LABEL) + # have tick marks for each session
+    #scale_x_continuous(X_LABEL, breaks = loc.xbreaks) + # have tick marks for each session
+    #with log scale
+    scale_x_discrete(X_LABEL) +
+    #scale_x_continuous(X_LABEL, breaks = loc.xbreaks, trans="log2") + # have tick marks for each session
+    #scale_y_continuous(Y_LABEL) + # have tick marks for each session
+    scale_y_continuous(Y_LABEL, limits = loc.ylimits ) + # have tick marks for each session
+    scale_shape_manual(values=SERIES_MARKERS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    scale_color_manual(values=SERIES_COLORS)  +# explicitly have sham=fillable triangle, ACCX=fillable circle
+    # scale_fill_manual(values=SERIES_FILL_COLORS) +
+    # scale_fill_brewer(palette="Blues") +
+    #scale_fill_hue(c=45, l=85) +
+    #scale_fill_manual(values=c("#999999", "#E69F00"))
+    #scale_fill_manual(values=c("#CC0000", "#006600", "#669999", "#00CCCC",
+    #               "#660099", "#CC0066", "#FF9999", "#FF9900", 
+    #                "black", "black", "black", "black", "black")) +
+    theme_bw()+  # make the theme black-and-white rather than grey (do this before font changes, or it overrides them)
+    #ggtitle(loc.plt_title)+
+    theme(
+      plot.title = element_text(face=MYPLOT_PROPS['plot.title', 'face'], size=MYPLOT_PROPS['plot.title', 'size']), # use theme_get() to see available options
+      axis.title.x = element_text(face=MYPLOT_PROPS['axis.title.x', 'face'], size=MYPLOT_PROPS['axis.title.x', 'size']),
+      axis.title.y = element_text(face=MYPLOT_PROPS['axis.title.y', 'face'], size=MYPLOT_PROPS['axis.title.y', 'size'], angle=MYPLOT_PROPS['axis.title.y', 'angle']),
+      axis.text = loc.ax_txt, 
+      # panel.grid.major = element_blank(), # switch off major gridlines
+      # panel.grid.minor = element_blank(), # switch off minor gridlines
+      legend.position = LEGEND_POS, # manually position the legend (numbers being from 0,0 at bottom left of whole plot to 1,1 at top right)
+      legend.title = loc.lg_title, # switch off the legend title
+      legend.text = element_text(size=MYPLOT_PROPS['legend.text', 'size']),
+      legend.key.size = unit(MYPLOT_PROPS['legend.key', 'units'], "lines"),
+      legend.key = loc.lg_key, # switch off the rectangle around symbols in the legend
+      legend.background = element_rect(fill=alpha('white', 0.2))
+    )
+  
+  return(p1)
+}
+
 
 
 # Multiple plot function
